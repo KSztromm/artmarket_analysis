af487d035f91b7d2fc1fc02e9140636b218623c5
Tomasz Mrozik <tomasz.mrozik0@gmail.com>
2020-11-29 00:01:59 +0100

    Further work on ai scraper, added getting data from all auctions by clicking on each of them

--- src/app/ai.py
+++ src/app/ai.py
@@ -29,7 +29,7 @@ pwd=driver.find_element_by_id('user_password')
 pwd.send_keys(ai_pwd)
 login_button=driver.find_element_by_xpath("//input[@value='Zaloguj się']")
 login_button.click()
-driver.implicitly_wait(1)
+driver.implicitly_wait(10)
 try:
     last_page = 0
     page = 0
@@ -47,28 +47,41 @@ try:
 
         df = pd.DataFrame(columns=['autor', 'data_aukcji', 'tytul_rok', 'technika', 'rozmiar', 'status', 'cena_wywolawcza', 'cena_sprzedazy', 'nr_dziela'])
 
-        auctions = driver.find_elements_by_xpath("//div[@class='row auction details']")
-        for auction in auctions:
+        auctions = driver.find_elements_by_xpath("//div[@class='row auction details']//div[@class='row justify-content-between auction__actions no-gutters']")
+        print(f'Aukcji na stronie: {len(auctions)}')
+        for i in range(len(auctions)):
+            print(i)
+            if auctions[i].find_element_by_xpath(".//span").get_attribute('innerText') == "Zobacz wyniki":
+                button_wyniki = auctions[i].find_element_by_xpath(".//a").click()
+                time.sleep(1) #important for button load
+                driver.find_element_by_xpath("//button[@class='all-records']").click()
+                time.sleep(1)
+                painters = driver.find_elements_by_xpath("//div[starts-with(@id, 'artwork_')]")
+                print(len(painters))
+                for j in range(len(painters)):
+                    print(painters[j].find_element_by_xpath(".//span[@class='artist-name']").get_attribute('innerText'))
 
-            auction_house = auction.find_element_by_xpath("//h2[@class='headings--h2']").get_attribute('innerText')
-            print(auction_house)
+                driver.back()
+                driver.back()
+                auctions = driver.find_elements_by_xpath("//div[@class='row auction details']//div[@class='row justify-content-between auction__actions no-gutters']")
 
-            df_in = {
-                'autor':[],
-                'data_aukcji':[],
-                'tytul_rok':[],
-                'technika':[],
-                'rozmiar':[],
-                'status':[],
-                'cena_wywolawcza':[],
-                'cena_sprzedazy':[],
-                'nr_aukcji':[],
-                'nr_dziela':[]
-            }
-
-            
-            
-            #get_metadata(driver, df_in, 'autor', "//a[@class='auction_list_painter_name']", ".", att='title')
+                df_in = {
+                    'autor':[],
+                    'data_aukcji':[],
+                    'tytul_rok':[],
+                    'technika':[],
+                    'rozmiar':[],
+                    'status':[],
+                    'cena_wywolawcza':[],
+                    'cena_sprzedazy':[],
+                    'nr_aukcji':[],
+                    'nr_dziela':[]
+                }
+            else:
+                print('Brak uprawnien dla tej aukcji')
+                
+                
+                #get_metadata(driver, df_in, 'autor', "//a[@class='auction_list_painter_name']", ".", att='title')
 
 finally:
     driver.quit()
